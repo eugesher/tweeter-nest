a90f31ba8f0528e749c0cfbb621b273edfdd9cae
Evgenii Chernyshov <mailbox@eugesher.site>
2021-10-11 15:10:34 +0300

    setup migrations

---
+++ src/ormconfig.ts
@@ -0,0 +1,17 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'admin',
+  password: 'admin',
+  database: 'tweeter',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export default config;
